43def2f6dfea6bd47ea838e681f02ba03869a120
Safa ERDEN <safaerden@gmail.com>
2020-04-14 20:45:27 +0300

    added d

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,14 +1,14 @@
 window.onscroll = function () {
   scrollFunction();
 };
-/*
+
 function scrollFunction() {
-    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-        document.getElementById("header1").style.display = "none";
-        document.getElementById("header2").style.display = "block";
-    } else {
-        document.getElementById("header1").style.display = "block";
-        document.getElementById("header2").style.display = "none";
-    }
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.getElementById("nav").style.display = "inline-flex";
+  } else {
+    document.getElementById("nav").style.display = "none";
   }
-  */
+}
